d79207fe4e9394b27709b7432e967c6fa2156782
Sahilydv7 <dk5531543@gmail.com>
2022-09-02 11:26:24 +0530

    product card screen gridview added

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -7,16 +7,16 @@ import '../constants/api_service.dart';
 import '../model/product_model.dart';
 
 class ProductCard extends StatefulWidget {
-  const ProductCard({Key? key, required this.index}) : super(key: key);
-  final int? index;
+  const ProductCard({Key? key}) : super(key: key);
+  // final int? index;
   @override
-  State<ProductCard> createState() => _ProductCardState(index);
+  State<ProductCard> createState() => _ProductCardState();
 }
 
 class _ProductCardState extends State<ProductCard> {
   late List<ProductModel>? _productModel = [];
-  final int? index;
-  _ProductCardState(this.index);
+  // final int? index;
+  // _ProductCardState(this.index);
 
   @override
   void initState() {
@@ -34,106 +34,117 @@ class _ProductCardState extends State<ProductCard> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        // ignore: avoid_print
-        print("card pressed");
-      },
-      child: Container(
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15.0),
-          ),
-          // color: Colors.grey,
-          elevation: 5,
-          child: Column(
-            children: [
-              Stack(
+    return GridView.builder(
+      itemCount: _productModel?.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () {
+            // ignore: avoid_print
+            print("card pressed");
+          },
+          child: Container(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              // color: Colors.grey,
+              elevation: 5,
+              child: Column(
                 children: [
-                  SizedBox(
-                    height: 230,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Image.network(
-                              fit: BoxFit.contain,
-                              height: 180,
-                              '${_productModel![index!].image}'),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
+                  Stack(
                     children: [
-                      _productModel![index!].isFavourite == false
-                          ? IconButton(
-                              icon: const Icon(Icons.favorite_outline,
-                                  color: Color.fromARGB(255, 255, 17, 0)),
-                              onPressed: () {
-                                setState(() {
-                                  _productModel![index!].favourite();
-                                });
-                              },
-                            )
-                          : IconButton(
-                              icon: const Icon(Icons.favorite,
-                                  color: Color.fromARGB(255, 255, 17, 0)),
-                              onPressed: () {
-                                setState(() {
-                                  _productModel![index!].favourite();
-                                });
-                              },
-                            )
+                      SizedBox(
+                        height: 230,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Image.network(
+                                  fit: BoxFit.contain,
+                                  height: 180,
+                                  '${_productModel![index].image}'),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          _productModel![index].isFavourite == false
+                              ? IconButton(
+                                  icon: const Icon(Icons.favorite_outline,
+                                      color: Color.fromARGB(255, 255, 17, 0)),
+                                  onPressed: () {
+                                    setState(() {
+                                      _productModel![index].favourite();
+                                    });
+                                  },
+                                )
+                              : IconButton(
+                                  icon: const Icon(Icons.favorite,
+                                      color: Color.fromARGB(255, 255, 17, 0)),
+                                  onPressed: () {
+                                    setState(() {
+                                      _productModel![index].favourite();
+                                    });
+                                  },
+                                )
+                        ],
+                      ),
                     ],
                   ),
-                ],
-              ),
-              Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(
-                      "${_productModel![index!].title}",
-                      softWrap: true,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Color.fromARGB(255, 118, 111, 111),
-                      ),
-                    ),
-                  ),
-                  Text(
-                    "${_productModel![index!].price}",
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  )
-                ],
-              ),
-              ElevatedButton(
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
+                  Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          "${_productModel![index].title}",
+                          softWrap: true,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromARGB(255, 118, 111, 111),
+                          ),
                         ),
                       ),
-                      padding: MaterialStateProperty.all(
-                        const EdgeInsets.fromLTRB(40, 0, 40, 0),
-                      ),
-                      backgroundColor: MaterialStateProperty.all(
-                          Color.fromARGB(255, 156, 155, 155))),
-                  onPressed: () {},
-                  child: const Text("Add to Cart"))
-            ],
+                      Text(
+                        "${_productModel![index].price}",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      )
+                    ],
+                  ),
+                  ElevatedButton(
+                      style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          padding: MaterialStateProperty.all(
+                            const EdgeInsets.fromLTRB(40, 0, 40, 0),
+                          ),
+                          backgroundColor: MaterialStateProperty.all(
+                              Color.fromARGB(255, 156, 155, 155))),
+                      onPressed: () {},
+                      child: const Text("Add to Cart"))
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          mainAxisExtent: 350,
+          childAspectRatio: 2 / 3,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10),
     );
-    ;
   }
 }
