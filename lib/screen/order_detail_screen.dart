3b946603dc1c0d5deba916817872f61f6595133b
Sahilydv7 <dk5531543@gmail.com>
2022-09-05 18:03:55 +0530

    Change address and payment method

---
+++ lib/screen/order_detail_screen.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+import 'package:im_stepper/stepper.dart';
+
+class OrderDetailScreen extends StatelessWidget {
+  const OrderDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      backgroundColor: Color.fromARGB(255, 152, 36, 188),
+      body: Column(children: [
+        Row(
+          children: [
+            IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.arrow_back_ios)),
+            const Text(
+              'Orders',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 60,
+        ),
+        ClipRRect(
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(50), topRight: Radius.circular(50)),
+          child: Container(
+            height: 642.1,
+            color: Color.fromARGB(255, 225, 218, 218),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                SizedBox(
+                  height: 592.1,
+                  child: ListView.builder(
+                    itemBuilder: ((context, index) => Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  'ID: 95 259105',
+                                  style: TextStyle(
+                                      color: Color.fromARGB(255, 152, 36, 188),
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: const [
+                                    Text(
+                                      '17 Sep, 22',
+                                      style: TextStyle(color: Colors.grey),
+                                    ),
+                                    Text(
+                                        style: TextStyle(color: Colors.grey),
+                                        '25 Sep, 22'),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: const [
+                                    Text(
+                                      'Delhi',
+                                      style: TextStyle(
+                                          color:
+                                              Color.fromARGB(255, 122, 36, 188),
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(
+                                      'Hisar',
+                                      style: TextStyle(
+                                          color:
+                                              Color.fromARGB(255, 122, 36, 188),
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                                IconStepper(
+                                  icons: const [
+                                    Icon(Icons.abc),
+                                    Icon(Icons.adjust),
+                                    Icon(Icons.access_alarm),
+                                    Icon(Icons.supervised_user_circle),
+                                  ],
+
+                                  // activeStep property set to activeStep variable defined above.
+                                  activeStep: index,
+                                  activeStepColor: Colors.blue,
+                                  stepRadius: 16,
+                                  lineColor: Colors.black,
+                                  steppingEnabled: false,
+                                  enableNextPreviousButtons: false,
+                                ),
+                                Card(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                      side:
+                                          BorderSide(color: Colors.blueAccent)),
+                                  child: Row(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(16.0),
+                                        child: Image.network(
+                                          'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+                                          height: 50,
+                                          width: 50,
+                                          fit: BoxFit.scaleDown,
+                                        ),
+                                      ),
+                                      Column(
+                                        children: [
+                                          Text(
+                                            'Bag',
+                                            style: const TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          Text(
+                                            'x${index + 1}',
+                                            style: const TextStyle(
+                                              fontSize: 16,
+                                              color: Colors.grey,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                      const Expanded(child: SizedBox()),
+                                      ClipRRect(
+                                        borderRadius: const BorderRadius.only(
+                                            topRight: Radius.circular(15),
+                                            bottomRight: Radius.circular(15)),
+                                        child: Container(
+                                          height: 82,
+                                          width: 82,
+                                          color: Colors.blueAccent,
+                                          child: Center(
+                                              child: Text(
+                                            'Rs. 1500',
+                                            style: const TextStyle(
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.bold),
+                                          )),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        )),
+                    itemCount: 5,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ]),
+    ));
+  }
+}
--- lib/screen/payment_screen.dart
+++ lib/screen/payment_screen.dart
@@ -1,14 +1,80 @@
 import 'package:e_commerce_app/main.dart';
 import 'package:e_commerce_app/screen/order_detail_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 
-class PaymentScreen extends StatelessWidget {
+class PaymentScreen extends StatefulWidget {
   const PaymentScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PaymentScreen> createState() => _PaymentScreenState();
+}
+
+enum PaymentMethod { masterCard, googlePay, phonePay, visaCard }
+
+class _PaymentScreenState extends State<PaymentScreen> {
+  final houseTextField = TextEditingController();
+  final floorTextField = TextEditingController();
+  final cityTextField = TextEditingController();
+  final countryTextField = TextEditingController();
+  String house = '727';
+  String floor = 'Second Floor';
+  String city = 'New York';
+  String country = 'USA';
+  String currentAddress = '';
+  late Position currentPosition;
+
+  Future<Position?> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      Fluttertoast.showToast(msg: "Please keep your location on");
+    }
+    permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        Fluttertoast.showToast(msg: 'Location Permission Denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      Fluttertoast.showToast(msg: 'Location permission is denied Forever');
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    try {
+      List<Placemark> placemarks =
+          await placemarkFromCoordinates(position.latitude, position.longitude);
+      Placemark place = placemarks[0];
+      setState(() {
+        currentPosition = position;
+        currentAddress =
+            '${place.locality},${place.postalCode},${place.country}';
+        house = place.name!;
+        city = place.locality!;
+        country = place.country!;
+        floor = place.postalCode!;
+      });
+    } catch (e) {
+      print(e);
+    }
+    return null;
+  }
+
+  PaymentMethod? _character = PaymentMethod.masterCard;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(colors: [
@@ -68,26 +134,109 @@ class PaymentScreen extends StatelessWidget {
                     padding: const EdgeInsets.all(8.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: const [
-                        Text(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
                           'Home',
                           style: TextStyle(
                               fontSize: 16, fontWeight: FontWeight.bold),
                         ),
                         Text(
-                          '727, Second Floor',
+                          '$house,$floor',
                           overflow: TextOverflow.ellipsis,
                           maxLines: 3,
-                          style: TextStyle(color: Colors.grey),
+                          style: const TextStyle(color: Colors.grey),
                         ),
                         Text(
-                          'New York',
-                          style: TextStyle(color: Colors.grey),
+                          city,
+                          style: const TextStyle(color: Colors.grey),
                         ),
                         Text(
-                          'USA',
-                          style: TextStyle(color: Colors.grey),
-                        )
+                          country,
+                          style: const TextStyle(color: Colors.grey),
+                        ),
+                        TextButton(
+                            style: ButtonStyle(
+                                padding:
+                                    MaterialStateProperty.all(EdgeInsets.zero)),
+                            onPressed: () {
+                              _determinePosition();
+                            },
+                            child: const Text('Use Current Location')),
+                        TextButton(
+                            style: ButtonStyle(
+                                padding:
+                                    MaterialStateProperty.all(EdgeInsets.zero)),
+                            onPressed: () {
+                              showModalBottomSheet(
+                                  isScrollControlled: true,
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return SingleChildScrollView(
+                                      padding:
+                                          MediaQuery.of(context).viewInsets,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.end,
+                                        children: [
+                                          TextFormField(
+                                            controller: houseTextField,
+                                            textInputAction:
+                                                TextInputAction.next,
+                                            decoration: const InputDecoration(
+                                                labelText: 'House No.'),
+                                          ),
+                                          TextFormField(
+                                            controller: floorTextField,
+                                            textInputAction:
+                                                TextInputAction.next,
+                                            decoration: const InputDecoration(
+                                                labelText: 'Floor (Optional)'),
+                                          ),
+                                          TextFormField(
+                                            controller: cityTextField,
+                                            textInputAction:
+                                                TextInputAction.next,
+                                            decoration: const InputDecoration(
+                                                labelText: 'City'),
+                                          ),
+                                          TextFormField(
+                                            controller: countryTextField,
+                                            textInputAction:
+                                                TextInputAction.done,
+                                            decoration: const InputDecoration(
+                                                labelText: 'Country'),
+                                          ),
+                                          const SizedBox(
+                                            height: 10,
+                                          ),
+                                          ElevatedButton(
+                                              onPressed: () {
+                                                setState(() {
+                                                  house = houseTextField.text;
+                                                  floor = floorTextField.text;
+                                                  city = cityTextField.text;
+                                                  country =
+                                                      countryTextField.text;
+                                                  countryTextField.clear();
+                                                  houseTextField.clear();
+                                                  floorTextField.clear();
+                                                  cityTextField.clear();
+                                                });
+                                                Navigator.pop(context);
+                                              },
+                                              style: ButtonStyle(
+                                                  backgroundColor:
+                                                      MaterialStateProperty.all(
+                                                          Colors.black)),
+                                              child:
+                                                  const Text('Save Address')),
+                                        ],
+                                      ),
+                                    );
+                                  });
+                            },
+                            child: const Text('Change Address'))
                       ],
                     ),
                   )
@@ -149,7 +298,74 @@ class PaymentScreen extends StatelessWidget {
                     child: IconButton(
                       icon: const Icon(Icons.arrow_forward_ios,
                           color: Colors.white),
-                      onPressed: () {},
+                      onPressed: () {
+                        showModalBottomSheet(
+                            context: context,
+                            builder: (BuildContext) {
+                              return Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Padding(
+                                    padding: EdgeInsets.fromLTRB(30, 8, 0, 8),
+                                    child: Text(
+                                      'Choose Your Payment Methord',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  ListTile(
+                                    title: const Text('MasterCard'),
+                                    leading: Radio<PaymentMethod>(
+                                      value: PaymentMethod.masterCard,
+                                      groupValue: _character,
+                                      onChanged: (PaymentMethod? value) {
+                                        setState(() {
+                                          _character = value;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                  ListTile(
+                                    title: const Text('GooglePay'),
+                                    leading: Radio<PaymentMethod>(
+                                      value: PaymentMethod.googlePay,
+                                      groupValue: _character,
+                                      onChanged: (PaymentMethod? value) {
+                                        setState(() {
+                                          _character = value;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                  ListTile(
+                                    title: const Text('PhonePay'),
+                                    leading: Radio<PaymentMethod>(
+                                      value: PaymentMethod.phonePay,
+                                      groupValue: _character,
+                                      onChanged: (PaymentMethod? value) {
+                                        setState(() {
+                                          _character = value;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                  ListTile(
+                                    title: const Text('Visa Card'),
+                                    leading: Radio<PaymentMethod>(
+                                      value: PaymentMethod.visaCard,
+                                      groupValue: _character,
+                                      onChanged: (PaymentMethod? value) {
+                                        setState(() {
+                                          _character = value;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              );
+                            });
+                      },
                     ),
                   ),
                   const SizedBox(
@@ -241,7 +457,7 @@ class PaymentScreen extends StatelessWidget {
                     style: ButtonStyle(
                         elevation: MaterialStateProperty.all(10),
                         backgroundColor: MaterialStateProperty.all(
-                            Color.fromARGB(220, 0, 0, 0)),
+                            const Color.fromARGB(220, 0, 0, 0)),
                         shape: MaterialStateProperty.all(RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(15.0),
                         ))),
